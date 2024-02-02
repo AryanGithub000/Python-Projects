410c231842f8aee54fe5e8f42aa54f84fdd4a5a5
Aryan Singh <103366393+AryanGithub000@users.noreply.github.com>
2024-02-03 00:19:05 +0530

    Add files via upload

---
+++ Rock_Paper_Scissor-(Easy).py
@@ -0,0 +1,44 @@
+import random
+
+user_wins=0
+computer_wins=0
+
+options =["rock","paper","scissor"]
+
+while True:
+    user_input=input("Type rock/paper/scissor or Q to quit: ").lower()
+    if user_input == "q":
+        break #exit the loop
+    
+    if user_input not in options:
+        continue #go back to the start of the loop
+
+    random_number = random.randint(0,2)
+    # rock 0 ; paper 1 ; scissor 2
+    computer_pick = options[random_number] # access the random number at list
+    print("Computer picked",computer_pick + ".")
+
+    if user_input == "rock" and computer_pick == "scissor":
+        print("You won!")
+        user_wins+=1
+
+    elif user_input == "paper" and computer_pick == "rock":
+        print("You won!")
+        user_wins+=1
+
+    elif user_input == "scissor" and computer_pick == "paper":
+        print("You won!")
+        user_wins+=1
+
+    else:
+        print("You lost!")
+        computer_wins+=1
+
+print("Goodbye!")
+print("You won",user_wins,"times!")
+
+    
+
+        
+
+    
